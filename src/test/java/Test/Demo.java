8f7bd0081963cf79cef4cf4f6b31a73e7b7d17d1
SHYAM <SHYAM@SHYAM-PC>
2020-01-29 16:57:00 +0530

    ActiTimeDemoFirstproject

---
+++ src/test/java/Test/Demo.java
@@ -0,0 +1,13 @@
+package Test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+@Test
+public void testA()
+{
+	Reporter.log("Maven project", true);
+}
+}
